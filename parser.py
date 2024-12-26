69c00bd4db5be682899836062e04b6a2eac97fba
Gökcan Çantalı <gokcantali@gmail.com>
2024-12-26 02:11:11 +0100

    Improve the parser by adding a file printing function so that summaries can be viewed in a human-interpretable way

--- parser.py
+++ parser.py
@@ -2,8 +2,8 @@ import copy
 import re, json
 
 
-def parse_file_with_confusion_matrices(file_name):
-    file_content = open(file_name, "r").read()
+def parse_file_with_confusion_matrices(f_name: str):
+    file_content = open(f_name, "r").read()
 
     match_pattern = r"\s*(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s*"
     replace_pattern = r"\1,\2,\3,\4"
@@ -70,13 +70,17 @@ def parse_single_confusion_matrix_and_obtain_metrics(conf_matrix: list[list[int]
             model_perf_metrics["class"][label]["fn"],
         )
 
-        if tp + fp == 0:
+        if tp + fn == 0:
             print(
                 f"WARNING: No sample for the class {label}, taking 1.0 for the metrics"
             )
             model_perf_metrics["class"][label]["pre"] = 1.0
             model_perf_metrics["class"][label]["rec"] = 1.0
             model_perf_metrics["class"][label]["f1"] = 1.0
+        elif tp == 0:
+            model_perf_metrics["class"][label]["pre"] = 0.0
+            model_perf_metrics["class"][label]["rec"] = 0.0
+            model_perf_metrics["class"][label]["f1"] = 0.0
         else:
             model_perf_metrics["class"][label]["pre"] = pre = tp / (tp + fp)
             model_perf_metrics["class"][label]["rec"] = rec = tp / (tp + fn)
@@ -142,16 +146,73 @@ def get_average_metrics_across_experiments(list_of_model_perf_metrics: list[dict
     return average_metrics
 
 
+def print_avg_performance_metrics_into_file(avg_perf_metrics: dict, f_name: str):
+    class_metrics = ["pre", "rec", "f1"]
+    general_metrics = [
+        "acc\t\t\t", "weight_avg_pre", "simple_avg_pre",
+        "weight_avg_rec", "simple_avg_rec", "weight_avg_f1", "simple_avg_f1"
+    ]
+    with open(f_name, "w") as file:
+        file.write("Metric\t\t\t\tBenign\t\t\t\t\tDoS\t\t\t\t\t\tPort\t\t\t\t\tZap\t\t\t\t\t\tGeneral\n")
+        for metric in class_metrics:
+            file.write(metric+"\t\t\t\t\t")
+            file.write(str(avg_perf_metrics["class"]["benign"][metric])+"\t\t")
+            file.write(str(avg_perf_metrics["class"]["dos"][metric]) + "\t\t")
+            file.write(str(avg_perf_metrics["class"]["port"][metric]) + "\t\t")
+            file.write(str(avg_perf_metrics["class"]["zap"][metric]) + "\t\t")
+            file.write("\n")
+        file.write("\n")
+        for metric in general_metrics:
+            file.write(metric+"\t\t")
+            file.write("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t")
+            file.write(str(avg_perf_metrics["general"][metric.strip()]))
+            file.write("\n")
+
+
 if __name__ == "__main__":
-    file_name = "train-worker0-test-worker0-FL-60-CF-ExpSmooth.txt"
-    conf_matrices = parse_file_with_confusion_matrices(file_name)
+    # input_file_name = "experiment/train-worker0-test-worker0-FL-60-CF-ExpSmooth.txt"
+    # conf_matrices = parse_file_with_confusion_matrices(input_file_name)
+    #
+    # print("Metrics for each Experiment:")
+    # list_of_perf_metrics = []
+    # for c_matrix in conf_matrices:
+    #     perf_metrics = parse_single_confusion_matrix_and_obtain_metrics(c_matrix)
+    #     print(perf_metrics)
+    #     list_of_perf_metrics.append(perf_metrics)
+    #
+    # print("\nAvg Metrics across Experiments:")
+    # average_performance_metrics = get_average_metrics_across_experiments(list_of_perf_metrics)
+    # print(average_performance_metrics)
+    #
+    # output_file_name = "performance-results-train-worker0-test-worker0-FL-60-CF-ExpSmooth.txt"
+    # print_avg_performance_metrics_into_file(
+    #     average_performance_metrics, output_file_name
+    # )
+    suffixes = [
+        "", "-FL-60-CF", "-FL-60-CF-ExpSmooth",
+        "-FL-60-CF-LinRegress", "-FL-60-NON-CF"
+    ]
 
-    print("Metrics for each Experiment:")
-    list_of_perf_metrics = []
-    for c_matrix in conf_matrices:
-        perf_metrics = parse_single_confusion_matrix_and_obtain_metrics(c_matrix)
-        print(perf_metrics)
-        list_of_perf_metrics.append(perf_metrics)
+    for train_ind in range(0, 5):
+        for test_ind in range(0, 5):
+            for suffix in suffixes:
+                input_file_name = (
+                    "results_phase2/experiment/" +
+                    f"train-worker{train_ind}-" +
+                    f"test-worker{test_ind}" +
+                    f"{suffix}.txt"
+                )
+                output_file_name = (
+                    "results_phase2/summary/" +
+                    f"summary-train-worker{train_ind}-" +
+                    f"test-worker{test_ind}" +
+                    f"{suffix}.txt"
+                )
 
-    print("\nAvg Metrics across Experiments:")
-    print(get_average_metrics_across_experiments(list_of_perf_metrics))
+                conf_matrices = parse_file_with_confusion_matrices(input_file_name)
+                perf_metrics = [
+                    parse_single_confusion_matrix_and_obtain_metrics(c_matrix)
+                    for c_matrix in conf_matrices
+                ]
+                avg_perf_metrics = get_average_metrics_across_experiments(perf_metrics)
+                print_avg_performance_metrics_into_file(avg_perf_metrics, output_file_name)
