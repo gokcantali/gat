5412eab84021fdcd227b78dd79a31ece20936210
Gökcan Çantalı <gokcantali@gmail.com>
2025-02-04 00:08:13 +0100

    Adjust the FL automation script to run as a cronjob without problem (hopefully)

--- start_fl_simulation.sh
+++ start_fl_simulation.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Source the user environment
+source ~/.bashrc
+
 # adjust the FL configuration parameters before the simulation
 sed -i.bak -E "s/NUM_ROUNDS = (.+)/NUM_ROUNDS = $1/g" ./fedl/fedl/server_app.py
 sed -i.bak -E "s/METHOD = \"(.+)\"/METHOD = \"$2\"/g" ./fedl/fedl/server_app.py
@@ -7,5 +12,4 @@ sed -i.bak -E "s/TRIAL = \"(.+)\"/TRIAL = \"$3\"/g" ./fedl/fedl/server_app.py
 # the superlink and the server-app are hosted on the same machine
 poetry run flower-server-app fedl \
       --superlink 0.0.0.0:9091 \
-      --insecure \
-      > FL_$1_Rounds_$2_Algorithm_$3_Trial_Results.txt &
+      --insecure >> FL_$1_Rounds_$2_Algorithm_$3_Trial_Results.txt 2>&1
