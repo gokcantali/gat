ad94e48d7f575ae50f9106460dc063c821a6bcf0
Gökcan Çantalı <gokcantali@gmail.com>
2025-03-07 15:12:21 +0100

    Add AirFlow configuration and the DAG for creating subsets of the primary dataset based on Monte-Carlo simulation. Implement the subsampling task to be used by the dataset DAG. Add a dummy environment file to represent an example configuration. Put 3rd party dependencies in the requirements file, because AirFlow installation with Poetry is problematic.

--- gat/load_data.py
+++ gat/load_data.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from torch import save, load
 from torch_geometric.data import Data
+
+PROJECT_ROOT = Path(__file__).parent.parent
 
 
 def load_data(file_path: Path = Path("data/sampled-traces-3ddos-2zap-1scan.csv"), sampling_ratio=1.0):
@@ -21,3 +24,44 @@ def save_graph_data(data: Data, name = 'traces-3ddos-2zap-1scan.pt'):
 def load_graph_data(name = 'traces-3ddos-2zap-1scan.pt'):
     file_path = Path(f"data/graph/{name}")
     return load(file_path)
+
+
+def create_subset_from_dataset_using_monte_carlo(file_name: str):
+    print("File Name: ", file_name)
+    file_path = Path(f"{PROJECT_ROOT}/data/{file_name}")
+
+    # Load the data
+    df = load_data(file_path=file_path, sampling_ratio=1.0)
+
+    # Randomly sample a subset of the data using random ratios for each class
+    benign_ratio = np.random.uniform(0.2, 1)
+    dos_ratio = np.random.uniform(0.3, 1)
+    port_scan_ratio = np.random.uniform(0.5, 1)
+    zap_scan_ratio = np.random.uniform(1, 1)
+
+    # Sample the data based on the drawn ratios
+    df_benign = df[df["anomaly_class"] == 0].sample(frac=benign_ratio)
+    df_dos = df[df["anomaly_class"] == 1].sample(frac=dos_ratio)
+    df_port_scan = df[df["anomaly_class"] == 2].sample(frac=port_scan_ratio)
+    df_zap_scan = df[df["anomaly_class"] == 3].sample(frac=zap_scan_ratio)
+
+    # Combine the dataframes
+    df_subset = pd.concat([df_benign, df_dos, df_port_scan, df_zap_scan])
+
+    # Sort by timestamp
+    df_subset = df_subset.sort_values(by=['timestamp'])
+
+    # Construct the file name based on the ratios
+    file_name = "traces-"
+    file_name += f"benign{benign_ratio:.2f}-"
+    file_name += f"dos{dos_ratio:.2f}-"
+    file_name += f"port{port_scan_ratio:.2f}-"
+    file_name += f"zap{zap_scan_ratio:.2f}.csv"
+
+    # Save the subset to a new file
+    df_subset.to_csv(f"{PROJECT_ROOT}/data/subsample/{file_name}", index=False)
+
+    return df_subset
+
+
+# create_subset_from_dataset_using_monte_carlo(Path(f"{PROJECT_ROOT}/data/sampled-traces-3ddos-2zap-1scan.csv"))
